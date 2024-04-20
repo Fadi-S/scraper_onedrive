886f73d4ba61b29a3ec50bd925d9de625b36bafd
Fadi Sarwat <fadisarwat@yahoo.com>
2024-04-20 18:45:43 +0200

    Allow for the use of username and password

--- get_token.py
+++ get_token.py
@@ -1,12 +1,14 @@
 import json
 import time
-from azure.identity import InteractiveBrowserCredential
+from azure.identity import InteractiveBrowserCredential, UsernamePasswordCredential
 
 
-def get_token():
+def get_token(from_console=True):
     keys = json.load(open("keys.json"))
     tenant_id = keys["tenant"]
     client_id = keys["client"]
+    email = keys["email"]
+    password = keys["password"]
 
     redirect_uri = "http://localhost:8080/auth/callback"
     scopes = ['Files.ReadWrite.All']
@@ -17,7 +19,10 @@ def get_token():
             raise Exception("Token expired")
         return token["token"]
     except:
-        credential = InteractiveBrowserCredential(client_id=client_id, tenant_id=tenant_id, redirect_uri=redirect_uri)
+        if from_console:
+            credential = UsernamePasswordCredential(client_id, username=email, password=password, tenant_id=tenant_id)
+        else:
+            credential = InteractiveBrowserCredential(client_id=client_id, tenant_id=tenant_id, redirect_uri=redirect_uri)
         access_token = credential.get_token(' '.join(scopes))
         with open("access_token.json", "w") as f:
             f.write(json.dumps({"token": access_token.token, "expires_on": access_token.expires_on}))
