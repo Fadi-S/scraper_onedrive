6444a442445721ecbd1070d687c56caba720219a
Fadi Sarwat <fadisarwat@yahoo.com>
2024-04-22 02:26:05 +0200

    WIP

--- split_audio.py
+++ split_audio.py
@@ -3,80 +3,50 @@ import json
 from pydub import AudioSegment
 import csv
 from tqdm import tqdm
-import threading
 
-# Define a lock for synchronizing access to the index variable
-index_lock = threading.Lock()
+data = []
 
+export_path = "data/splitted_audio"
+os.makedirs(export_path, exist_ok=True)
 
-def process_folder(gender, folder, export_path, done):
-    global index
+done = []
+try:
+    with open("data/done.json", "r") as file:
+        done = json.load(file)
+except FileNotFoundError:
+    pass
 
-    if folder in done:
-        return
+index = 1
+try:
+    for gender in ["male", "female"]:
+        folders = os.listdir(f"data/{gender}")
 
-    path = f"data/{gender}/{folder}"
-    audio_path = f"{path}/audio.mp3"
-    transcript_path = f"{path}/transcript.json"
+        for folder in tqdm(folders):
+            if folder in done:
+                continue
+            path = f"data/{gender}/{folder}"
+            audio_path = f"{path}/audio.mp3"
+            transcript_path = f"{path}/transcript.json"
 
-    transcript = json.load(open(transcript_path, "r"))
-    audio = AudioSegment.from_file(audio_path)
-
-    data = []
-
-    with tqdm(transcript, desc=f"Processing folder '{folder}'") as pbar:
-        for line in pbar:
-            start = line["start"]
-            end = start + line["duration"]
-            # Split audio with python
-            split_audio = audio[start * 1000:end * 1000]
-
-            # Use the lock to synchronize access to the index variable
-            with index_lock:
+            transcript = json.load(open(transcript_path, "r"))
+            audio = AudioSegment.from_file(audio_path)
+            for line in tqdm(transcript):
+                start = line["start"]
+                end = start + line["duration"]
+                # Split audio with python
+                split_audio = audio[start * 1000:end * 1000]
                 new_path = f"{export_path}/{index}.mp3"
+                split_audio.export(new_path, format="mp3")
+
+                data.append([new_path, line["text"], gender])
                 index += 1
-
-            split_audio.export(new_path, format="mp3")
-
-            data.append([new_path, line["text"], gender])
-
-    with open(f'{export_path}/index.csv', 'a', newline='') as file:
+            done.append(folder)
+finally:
+    file_path = f'{export_path}/index.csv'
+    file_exists = os.path.isfile(file_path)
+    with open(file_path, 'a' if file_exists else 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
-    done.append(folder)
-
-
-def main():
-    export_path = "data/splitted_audio"
-    os.makedirs(export_path, exist_ok=True)
-
-    done = []
-    try:
-        with open("data/done.json", "r") as file:
-            done = json.load(file)
-    except FileNotFoundError:
-        pass
-
-    global index
-    index = 1
-
-    threads = []
-    for gender in ["male", "female"]:
-        folders = os.listdir(f"data/{gender}")
-        for folder in folders:
-            thread = threading.Thread(target=process_folder, args=(gender, folder, export_path, done))
-            thread.start()
-            threads.append(thread)
-
-    # Wait for all threads to complete
-    for thread in threads:
-        thread.join()
-
-    # Save the updated 'done' list to the JSON file
     with open("data/done.json", "w") as f:
         json.dump(done, f, indent=1)
-
-
-if __name__ == "__main__":
-    main()
