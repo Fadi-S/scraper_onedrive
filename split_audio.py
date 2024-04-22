259ca4c57112618158fd2558d2b7e5df7f99e17f
Fadi Sarwat <fadisarwat@yahoo.com>
2024-04-22 02:58:50 +0200

    Change string

--- split_audio.py
+++ split_audio.py
@@ -3,9 +3,20 @@ import json
 from pydub import AudioSegment
 import csv
 from tqdm import tqdm
+import random
+import string
 
 export_path = "data/splitted_audio"
 os.makedirs(export_path, exist_ok=True)
+
+
+def generate_random_string(length=6):
+    characters = string.ascii_letters + string.digits
+
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_string
+
 
 done = []
 try:
@@ -31,17 +42,17 @@ for gender in ["male", "female"]:
         data = []
 
         for line in tqdm(transcript):
+            filename = generate_random_string()
             start = line["start"]
             end = start + line["duration"]
             # Split audio with python
             split_audio = audio[start * 1000:end * 1000]
-            new_path = f"{export_path}/{index}.mp3"
+            new_path = f"{export_path}/{filename}.mp3"
             split_audio.export(new_path, format="mp3")
 
-            data.append([new_path, line["text"], gender])
-            index += 1
+            data.append([f"{filename}.mp3", line["text"], gender])
 
-        with open(index_path, 'w', newline='') as file:
+        with open(index_path, 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(data)
         done.append(folder)
