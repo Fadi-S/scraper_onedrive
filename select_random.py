77cafadbc83932145f9380fb261a0dbb0a9c2c1a
Fadi Sarwat <fadisarwat@yahoo.com>
2024-04-27 17:11:00 +0300

    WIP

--- select_random.py
+++ select_random.py
@@ -1,17 +1,29 @@
 import random
 import os
 import pandas as pd
+import shutil
+from tqdm import tqdm
 
 # Load index.csv
 index = pd.read_csv('data/index.csv')
 
-# Select 60,000 random samples
-random_samples = random.sample(range(len(index)), 70_000)
 
-# Save random samples to a new csv file
-index.iloc[random_samples].to_csv('data/random_samples.csv', index=False)
+# Separate female and male samples
+female = index[index['gender'] == 'female']
+male = index[index['gender'] == 'male']
 
-# Save random samples to a new folder
+male = male.sample(frac=1).reset_index(drop=True)
+
+male = male.iloc[:len(female)]
+
+stacked_samples = pd.concat([female, male], axis=0)
+
+# Create folder for random samples
 os.makedirs('data/random_samples', exist_ok=True)
-for i in random_samples:
-    os.system(f'cp data/audios/{index.iloc[i, 0]} data/random_samples/{index.iloc[i, 0]}')
+stacked_samples.to_csv('data/random_samples.csv', index=False)
+
+# Copy files to random_samples folder
+for filename in tqdm(stacked_samples['file_name']):
+    shutil.copy(f'data/audios/{filename}', f'data/random_samples/{filename}')
+
+print("Random samples copied to 'data/random_samples' folder.")
