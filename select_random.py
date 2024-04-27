2acc0a253f33ce0e9616b5cf8796408a126ec830
Fadi Sarwat <fadisarwat@yahoo.com>
2024-04-27 14:58:45 +0300

    Select random files from split audio

---
+++ select_random.py
@@ -0,0 +1,17 @@
+import random
+import os
+import pandas as pd
+
+# Load index.csv
+index = pd.read_csv('data/index.csv')
+
+# Select 60,000 random samples
+random_samples = random.sample(range(len(index)), 60000)
+
+# Save random samples to a new csv file
+index.iloc[random_samples].to_csv('data/random_samples.csv', index=False)
+
+# Save random samples to a new folder
+os.makedirs('data/random_samples', exist_ok=True)
+for i in random_samples:
+    os.system(f'cp data/audios/{index.iloc[i, 0]} data/random_samples/{index.iloc[i, 0]}')
